d1331acbe56c22946f0c23f1ce198c6b37b23d76
Michael Gilbert <gilbertm@mit.edu>
2025-04-30 17:10:37 -0400

    [looptree] Finish test for symbolic reuse analysis

--- tests/looptree/test_symbolic_reuse_analysis.py
+++ tests/looptree/test_symbolic_reuse_analysis.py
@@ -1,9 +1,7 @@
 import unittest
-from pathlib import Path
-from itertools import starmap
+from sympy import ceiling
 
-from bindings.looptree import *
-from tests.util import TEST_TMP_DIR
+from bindings.looptree import LooptreeWorkload, LooptreeWorkloadDependencyAnalyzer
 
 from tests.load_config_mixin import LoadConfigMixin
 
@@ -22,5 +20,20 @@ class TestSymbolicReuseAnalysis(unittest.TestCase, LoadConfigMixin):
         workload = LooptreeWorkload.parse_cfg(config.root['problem'])
         analyzer = LooptreeWorkloadDependencyAnalyzer(workload)
 
-        result = analyze_reuse(mapping, workload, analyzer)
-        print(result)
+        tile_shapes, result = analyze_reuse(mapping, workload, analyzer)
+
+        self.assertEqual(len(tile_shapes), 3)
+        P1_tile_shape, C1_tile_shape, M1_tile_shape = tile_shapes
+
+        REFERENCE_FILLS = {
+            ('DRAM', 0, 0): (None, 18),
+            ('DRAM', 1, 0): (None, 8),
+            ('DRAM', 2, 0): (None, 36),
+            ('GlobalBuffer', 0, 0): (None, 18.0*ceiling(4/M1_tile_shape)),
+            ('GlobalBuffer', 1, 0): (None, 8)
+        }
+
+        for key, ref_value in REFERENCE_FILLS.items():
+            self.assertEqual(result.fills[key],
+                             ref_value,
+                             f'fills for {key} do not match')
